033f2922d6c9e78267cefca1b9cdd5aa6e5f4e50
Alejandro <smarketing.cuz@gmail.com>
2023-08-06 15:51:00 -0500

    add link to home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Crypto Portfolio",
-  description: "A simple crypto portfolio app built with Next.js and TailwindCSS",
+  description:
+    "A simple crypto portfolio app built with Next.js and TailwindCSS",
 };
 
 export default function RootLayout({
@@ -15,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="text-gray-600 bg-gradient-to-b from-slate-100 to-white min-h-screen">
         <nav className="w-full bg-white py-4 px-12 border-b flex justify-between items-center">
-          Crypto Portfolio
+          <Link className="font-bold text-lg" href="/">Crypto Portfolio</Link>
           <button className="border rounded-lg py-2 px-5 hover:bg-gray-100">
             Connect Wallet
           </button>
